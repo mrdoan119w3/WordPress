06dfb20a1add5804a21c526a49012221130dcd32
mrdoan119w3 <mrdoan119w3@gmail.com>
2018-02-03 22:30:16 +0700

    Add files via upload

---
+++ Theme/Blank Theme/404.php
@@ -0,0 +1,9 @@
+<?php get_header();?>
+<div class="container">
+	<div class="row">
+		<div class="col-xs-12 text-center" style="padding: 30px 0; color: red;">
+			<h1>Oh No! That Page Doesn't Exist!</h1>
+		</div>
+	</div>
+</div>
+<?php get_footer();?>
